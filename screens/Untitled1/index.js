1daeed9c0f99a68cee2041d304d0dd0179571971
Jason Kozel <jason.kozel@crowdbotics.com>
2023-05-02 17:44:41 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,5 @@
+import { Text } from "react-native";
+import { View } from "react-native";
 import { ImageBackground } from "react-native";
 import { StyleSheet, SafeAreaView } from "react-native";
 
@@ -6,22 +8,49 @@ const Untitled1 = () => {
         
     <ImageBackground style={styles.DilvrmFk} source={{
       uri: "https://www.pepsi.com/en-us/uploads/images/twil-can.png"
-    }} resizeMode="cover"></ImageBackground></SafeAreaView>;
+    }} resizeMode="cover"></ImageBackground><View style={styles.YmZMBGQl}><Text style={styles.dSckYkYa}>{"GET STARTED"}</Text></View></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%',
-    backgroundColor: "#024FA9"
+    backgroundColor: "#04479E"
   },
   DilvrmFk: {
-    width: 107,
-    height: 62,
+    width: 356,
+    height: 486,
     transform: [{
       rotate: "0deg"
     }],
     top: 0,
     left: 0
+  },
+  YmZMBGQl: {
+    height: 86,
+    width: 320,
+    backgroundColor: "#04479E",
+    borderRadius: 0,
+    color: "#777777",
+    position: "relative",
+    left: 0,
+    top: 0,
+    transform: [{
+      rotate: "0deg"
+    }]
+  },
+  dSckYkYa: {
+    width: 244,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 30,
+    borderRadius: 0,
+    fontFamily: "Open Sans",
+    position: "absolute",
+    top: 35,
+    left: 56,
+    textAlign: "center",
+    fontWeight: "700",
+    color: "#ffffff"
   }
 });
 export default Untitled1;
