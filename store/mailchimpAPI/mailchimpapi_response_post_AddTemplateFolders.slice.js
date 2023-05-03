91880aed24c1247c0539055e781a69ccbcb3c008
Jason Kozel <jason.kozel@crowdbotics.com>
2023-05-03 15:55:23 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/mailchimpAPI/mailchimpapi_response_post_AddTemplateFolders.slice.js
+++ store/mailchimpAPI/mailchimpapi_response_post_AddTemplateFolders.slice.js
@@ -1,49 +1,41 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create = createAsyncThunk(
-  "mailchimpapi_response_post_AddTemplateFolders/mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create",
-  async payload => {
-    const response = await apiService.mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create(
-      payload
-    )
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create = createAsyncThunk("mailchimpapi_response_post_AddTemplateFolders/mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create", async payload => {
+  const response = await apiService.mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const mailchimpapi_response_post_AddTemplateFoldersSlice = createSlice({
   name: "mailchimpapi_response_post_AddTemplateFolders",
   initialState,
   reducers: {},
   extraReducers: {
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.pending]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.fulfilled]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
       }
     },
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.rejected]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   mailchimpapi_post_modules_mailchimp_templates_addtemplatefolder_create,
   slice: mailchimpapi_response_post_AddTemplateFoldersSlice
-}
+};
--- store/mailchimpAPI/mailchimpapi_response_post_AddTemplateInFolders.slice.js
+++ store/mailchimpAPI/mailchimpapi_response_post_AddTemplateInFolders.slice.js
@@ -1,49 +1,41 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const mailchimpapi_post_modules_mailchimp_templates_addtemplate_create = createAsyncThunk(
-  "mailchimpapi_response_post_AddTemplateInFolders/mailchimpapi_post_modules_mailchimp_templates_addtemplate_create",
-  async payload => {
-    const response = await apiService.mailchimpapi_post_modules_mailchimp_templates_addtemplate_create(
-      payload
-    )
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const mailchimpapi_post_modules_mailchimp_templates_addtemplate_create = createAsyncThunk("mailchimpapi_response_post_AddTemplateInFolders/mailchimpapi_post_modules_mailchimp_templates_addtemplate_create", async payload => {
+  const response = await apiService.mailchimpapi_post_modules_mailchimp_templates_addtemplate_create(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const mailchimpapi_response_post_AddTemplateInFoldersSlice = createSlice({
   name: "mailchimpapi_response_post_AddTemplateInFolders",
   initialState,
   reducers: {},
   extraReducers: {
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.pending]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.fulfilled]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
       }
     },
-    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.rejected]: (
-      state,
-      action
-    ) => {
+    [mailchimpapi_post_modules_mailchimp_templates_addtemplate_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   mailchimpapi_post_modules_mailchimp_templates_addtemplate_create,
   slice: mailchimpapi_response_post_AddTemplateInFoldersSlice
-}
+};
